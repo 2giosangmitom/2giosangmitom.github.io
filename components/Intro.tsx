7bb9ed05902dfb3a1dadcc6f325ee01231d977ac
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-21 21:59:49 +0700

    feat: add intro component

---
+++ components/Intro.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div>
+      <h1 className="text-white text-5xl font-bold">Hi, I&apos;m Chien</h1>
+      <h2 className="text-yellow-50 text-3xl font-medium">Full-stack developer</h2>
+    </div>
+  )
+}
