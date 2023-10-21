dbcf197f4682d78eb2fb4a0d5af5e3c23b977c43
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-21 23:19:56 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5 }}>
+    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
       <Intro />
     </motion.main>
   )
