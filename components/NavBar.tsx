2a92b79195694da54f8ba03dedd1a5f6511603aa
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-21 22:27:22 +0700

    update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { navItems } from '@/lib/datas'
+import Link from 'next/link'
 
-export default function NavBar() {
+export default function NavBar(): JSX.Element {
   return (
     <nav className="h-20">
-      <ul></ul>
+      <ul className="sm:flex sm:justify-end sm:h-full sm:items-center">
+        {navItems.map((item, index) => (
+          <li key={index} className="px-2">
+            <Link href={item.route} className="capitalize text-gray-200">
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
