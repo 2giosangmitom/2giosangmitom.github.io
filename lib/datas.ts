da856be09d4beaeb52836e6c2f86d17940e7df64
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-28 15:14:56 +0700

    add gh, linkedin link

--- lib/datas.ts
+++ lib/datas.ts
@@ -1,7 +1,12 @@
-import { navItem } from './types'
+import { navItem, socialMedia } from './types'
 
 export const navItems: navItem[] = [
   { name: 'home', route: '/' },
   { name: 'blog', route: '/blog' },
-  { name: 'about', route: '/about' },
+  { name: 'about', route: '/about' }
+]
+
+export const socialAccounts: socialMedia[] = [
+  { name: 'Github', link: 'https://github.com/2giosangmitom' },
+  { name: 'LinkedIn', link: 'https://www.linkedin.com/in/2giosangmitom/' }
 ]
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,3 +2,8 @@ export type navItem = {
   name: string
   route: string
 }
+
+export type socialMedia = {
+  name: string
+  link: string
+}
