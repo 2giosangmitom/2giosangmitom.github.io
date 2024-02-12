f26125cdbdb9258f5d0e9f99ef70f42029211cb0
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-12 20:58:23 +0700

    updates

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <span class="header__logo">2giosangmitom</span>
+  </header>
+</template>
+
+<style scoped>
+.header__logo {
+  font-size: 1.25rem;
+  color: var(--neutral-white);
+  user-select: none;
+}
+
+.header__logo:hover {
+  opacity: 0.9;
+  cursor: pointer;
+}
+</style>
