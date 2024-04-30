238201b97c1df987255a99462eac5d79b860364f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-04-30 10:55:59 +0700

    add cursor

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,3 +1,25 @@
+"use client"
+import { useEffect, useState } from "react";
+
 export default function () {
-  return <div className="" />;
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    return () => document.removeEventListener("mousemove", onMouseMove);
+  }, []);
+
+  const onMouseMove = (e: MouseEvent) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  return (
+    <div
+      className="fixed -z-10 transition w-[35rem] h-[35rem] bg-blue-950 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
 }
