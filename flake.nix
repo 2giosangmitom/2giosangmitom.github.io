84aa433ca559158615b6e98ff2d1f387cb41bb67
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-23 11:24:25 +0700

    chore: update

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Portfolio built with Nuxt.js to showcase skills and projects in an engaging way.";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        alejandra
+        nil
+        nodejs_20
+        corepack_20
+      ];
+    };
+    formatter.${system} = pkgs.alejandra;
+  };
+}
