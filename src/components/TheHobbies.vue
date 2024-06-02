01184a93d08df62e5146503095b8216e8502a188
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-06-02 13:47:55 +0700

    feat: improve hobbies content

--- src/components/TheHobbies.vue
+++ src/components/TheHobbies.vue
@@ -2,9 +2,19 @@
   <section class="hobbies">
     <h2 class="section-title">Hobbies & Interests</h2>
     <p>
-      I love to explore new technologies and try out new things. I'm also
-      interested in contributing to open-source projects. I enjoy gym,
-      calisthenics, and playing sports.
+      When I'm not coding, I enjoy going to the gym, practicing calisthenics,
+      and playing sports. These activities not only provide relaxation but also
+      contribute to my overall health.
     </p>
   </section>
 </template>
+
+<style scoped>
+.hobbies {
+  max-width: 60rem;
+}
+
+.hobbies p {
+  line-height: 24px;
+}
+</style>
