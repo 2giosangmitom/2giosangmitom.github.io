e8fedf1a3d1b5ad34541fd5a8efa158bb5b78470
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-02 13:35:24 +0700

    fix: make download CV button easier to click

--- src/components/TheIntro.vue
+++ src/components/TheIntro.vue
@@ -3,7 +3,7 @@
     <div class="avatar">
       <img
         src="../assets/images/avt.png"
-        alt="avatar"
+        alt="Avatar of Vo Quang Chien"
         width="100"
         height="100"
       />
@@ -15,11 +15,7 @@
       uncover insights into my journey and the diverse skill set I've honed
       along the way.
     </p>
-    <button>
-      <a href="/cv.pdf" target="_blank" rel="noopener noreferrer"
-        >Download CV</a
-      >
-    </button>
+    <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">Download CV</a>
   </section>
 </template>
 
@@ -37,7 +33,7 @@
   line-height: 24px;
 }
 
-.intro button {
+.intro a {
   display: inline-block;
   margin-top: 1rem;
   padding: 0.5rem 1rem;
@@ -46,16 +42,13 @@
   border-radius: 0.25rem;
   cursor: pointer;
   transition: background-color 0.2s ease-in-out;
-}
-
-.intro button:hover {
-  background-color: #333333;
-}
-
-.intro button a {
   color: #ffd0ec;
   text-decoration: none;
   font-size: 15px;
   font-weight: 600;
+}
+
+.intro a:hover {
+  background-color: #333333;
 }
 </style>
