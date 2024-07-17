964d5b822266cdcfb3f1ce7c78bb6f42ba736112
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-17 09:57:20 +0700

    setup pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -3,27 +3,21 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
+  outputs = { self, nixpkgs, flake-utils, git-hooks, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            alejandra
-            nil
-            nodejs_20
-            pnpm
-            typst
-            typstyle
-          ];
+        checks.pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = { prettier.enable = true; };
         };
-      }
-    );
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages
+            ++ (with pkgs; [ nodejs_20 typst ]);
+        };
+      });
 }
