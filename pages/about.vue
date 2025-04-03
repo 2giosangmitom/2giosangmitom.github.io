64a117d8f19317e9665a396b270387b5a93342f9
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-03 09:20:52 +0700

    feat: update about page

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,6 @@ const info = [
   { label: "Pronouns", value: "He/Him" },
   { label: "Nationality", value: "Vietnam" },
   { label: "Human Languages", value: "English, Vietnamese" },
-  { label: "Preferred Programming Languages", value: "C++, JavaScript" },
   { label: "Favorite Beer", value: "Huda" },
 ];
 
@@ -32,12 +31,12 @@ const skills = [
 <template>
   <div>
     <TheTitle text="about me" class="mb-4" />
-    <h2 class="text-xl font-semibold">Vo Quang Chien</h2>
-    <p class="text-accent">
-      <em>Always, I&apos;m a newbie...</em>
-    </p>
 
     <!-- Personal Info -->
+    <h2 class="text-xl font-semibold">Vo Quang Chien</h2>
+    <p class="text-accent mb-2">
+      <em>Always, I&apos;m a newbie...</em>
+    </p>
     <ul class="ml-5 space-y-1">
       <li
         v-for="item in info"
@@ -50,19 +49,14 @@ const skills = [
 
     <!-- Professional Skills -->
     <h2 class="text-xl font-semibold mt-6">Professional Skills</h2>
-    <div>
-      <div v-for="skill in skills" :key="skill.category" class="mt-2">
-        <h4 class="font-semibold text-accent">{{ skill.category }}</h4>
-        <ul class="ml-4 space-y-1">
-          <li
-            v-for="item in skill.items"
-            :key="item"
-            class="relative before:content-['•'] before:absolute before:-left-4 before:text-accent"
-          >
-            {{ item }}
-          </li>
-        </ul>
-      </div>
-    </div>
+    <ul class="ml-5 space-y-1">
+      <li
+        v-for="item in skills"
+        :key="item.category"
+        class="relative before:content-['-'] before:absolute before:-left-5 before:text-accent"
+      >
+        <b>{{ item.category }}:</b> {{ item.items.join(", ") }}
+      </li>
+    </ul>
   </div>
 </template>
