6f2117c57fdca7485fa6c2c42593513281abcde1
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-11 20:41:18 +0700

    feat: improve code block style

--- components/content/ProsePre.vue
+++ components/content/ProsePre.vue
@@ -1,11 +1,5 @@
-<template>
-  <pre :class="$props.class" class="bg-zinc-900 overflow-x-auto border border-zinc-800 rounded">
-    <slot />
-  </pre>
-</template>
-
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   code: {
     type: String,
     default: "",
@@ -31,7 +25,45 @@ defineProps({
     default: null,
   },
 });
+
+const copied = ref<boolean>(false);
+
+const handleCopy = async () => {
+  copied.value = true;
+
+  await navigator.clipboard.writeText(props.code);
+
+  setTimeout(() => {
+    copied.value = false;
+  }, 1000);
+};
 </script>
+
+<template>
+  <div class="relative">
+    <div v-show="!copied">
+      <UButton
+        class="absolute right-0 top-0 cursor-pointer outline-none rounded-none rounded-bl-sm dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-700 p-2 text-lg bg-zinc-300 text-black hover:bg-zinc-400"
+        icon="mdi-light:clipboard"
+        title="Copy code"
+        size="xl"
+        @click="handleCopy"
+      />
+    </div>
+    <div v-show="copied">
+      <UButton
+        class="absolute right-0 top-0 cursor-pointer outline-none rounded-none rounded-bl-sm dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-700 p-2 text-lg bg-zinc-300 text-black hover:bg-zinc-400"
+        icon="mdi-light:clipboard-check"
+        title="Copy code"
+        size="xl"
+      />
+    </div>
+    <pre
+      class="dark:bg-zinc-900 border dark:border-zinc-800 bg-zinc-100 border-zinc-300 my-2"
+      :class="$props.class"
+    ><slot/></pre>
+  </div>
+</template>
 
 <style>
 pre code .line {
