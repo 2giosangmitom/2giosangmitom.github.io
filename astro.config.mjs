0089148177d27c51da7a5e24b2e04446b6f76619
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-18 07:45:50 +0700

    feat: add vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2giosangmitom.is-a.dev',
-  integrations: [sitemap()]
+  integrations: [sitemap(), vue()]
 });
+
