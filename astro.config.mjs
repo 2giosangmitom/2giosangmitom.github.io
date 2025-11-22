9cf8fad9a14b405ef02e08a4271db2400ef92e10
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-22 11:12:27 +0700

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2giosangmitom.is-a.dev',
-  integrations: [sitemap(), vue()]
+  integrations: [sitemap(), vue()],
+  adapter: netlify({
+    edgeMiddleware: true
+  })
 });
+
