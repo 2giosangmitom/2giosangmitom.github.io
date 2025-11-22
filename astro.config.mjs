25b46395e0ae8e245ec75129ca6dbf9a60d8cdfb
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-23 06:44:48 +0700

    chore: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
 import netlify from '@astrojs/netlify';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2giosangmitom.is-a.dev',
-  integrations: [sitemap(), vue()],
+  integrations: [sitemap(), vue(), pagefind()],
   adapter: netlify({
     edgeMiddleware: true
   })
