535d034b37bd5450ac38945608acc6486facc83c
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-23 17:04:59 +0700

    chore: small update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vue from '@astrojs/vue';
 import netlify from '@astrojs/netlify';
 import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2giosangmitom.is-a.dev',
-  integrations: [sitemap(), vue(), pagefind()],
+  integrations: [sitemap(), pagefind()],
   adapter: netlify({
     edgeMiddleware: true
   })
