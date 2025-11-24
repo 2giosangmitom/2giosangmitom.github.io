72ca8d4afda88d9f56c2f62225ad50df36a10805
Vo Quang Chien <voquangchien.dev@proton.me>
2025-11-24 11:24:11 +0700

    chore: remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2giosangmitom.is-a.dev',
-  integrations: [sitemap(), pagefind()],
-  adapter: netlify({
-    edgeMiddleware: true
-  })
+  integrations: [sitemap(), pagefind()]
 });
